6750c4472550e8a8f75721eecacf71f6c2726c69
maybetree <maybetree@stratal.systems>
2026-02-25 18:37:44 +0100

    add scripts, update reqa

--- dockertest/test-debian-bookworm-base.sh
+++ dockertest/test-debian-bookworm-base.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+set -x
+
 docker run --rm -ti \
 	-v ./dist:/dist:ro \
-	gateau-debian-bookworm \
+	gateau-debian-bookworm-base \
 	sh -c '. /venv/bin/activate; pip install dist/*.tar.gz ; python -m gateau'
 
--- dockertest/test-ubuntu-20-04.sh
+++ dockertest/test-ubuntu-20-04.sh
@@ -3,5 +3,5 @@
 docker run --rm -ti \
 	-v ./dist:/dist:ro \
 	gateau-ubuntu-20-04 \
-	sh -c '. /venv/bin/activate; pip install /dist/*.whl ; python -m gateau'
+	sh -c '. /venv/bin/activate; pip install dist/*.tar.gz ; python -m gateau'
 
